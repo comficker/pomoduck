46383d2b93cf144386f0cdfecb4903207998d8c0
comficker <comficker@gmail.com>
2025-07-03 15:30:09 +0700

    Init

--- app.vue
+++ app.vue
@@ -69,12 +69,12 @@ watch(() => route.path, () => {
         </div>
       </div>
     </div>
-    <div class="max-w-md mx-auto flex-1 w-full relative">
+    <div class="max-w-md mx-auto flex-1 w-full relative mb-24">
       <div class="absolute inset-0 overflow-auto">
         <nuxt-page/>
       </div>
     </div>
-    <div class="md:fixed sticky bottom-0 md:bottom-auto md:top-0 inset-x-0 max-w-md mx-auto w-full relative z-10 bg-white p-4">
+    <div class="fixed bottom-0 md:bottom-auto md:top-0 inset-x-0 max-w-md mx-auto w-full z-10 bg-white p-4">
       <div class="relative">
         <div v-if="route.path == '/'" class="-z-10 absolute bottom-1 md:bottom-auto md:top-1 -inset-x-2">
           <div class="md:hidden flex justify-center" @click="store.openDrawer = !store.openDrawer">
