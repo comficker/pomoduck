88d752eeff63a342e35d361400af88ec48a66c3e
comficker <comficker@gmail.com>
2025-09-10 14:14:18 +0700

    Init

--- app.vue
+++ app.vue
@@ -54,7 +54,7 @@ onMounted(async () => {
     }
     if (['ios', 'android'].includes(WebApp.platform)) {
       WebApp.requestFullscreen()
-      document.body.style.setProperty("--head-top-extra", "80px")
+      document.body.style.setProperty("--head-top-extra", "100px")
     }
   }
 })
