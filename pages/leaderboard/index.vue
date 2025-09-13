73059245f24f63e18fb22424b52f14b58b61414b
comficker <comficker@gmail.com>
2025-09-13 09:37:35 +0700

    Init

--- pages/leaderboard/index.vue
+++ pages/leaderboard/index.vue
@@ -48,7 +48,7 @@ const {data: mate} = useAuthFetch<APIResponse<Account>>('/accounts/', {
         </div>
       </div>
     </div>
-    <div class="bg-white pt-4 space-y-3 sticky bottom-4">
+    <div class="bg-white pt-4 space-y-3 sticky bottom-0">
       <div class="relative">
         <div class="flex items-center gap-4">
           <input
