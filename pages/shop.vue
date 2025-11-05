c264f13fe819e8a87d0667868a08627c4f48f1ef
Simon <comficker@gmail.com>
2025-11-05 15:07:33 +0700

    Init

--- pages/shop.vue
+++ pages/shop.vue
@@ -15,33 +15,48 @@ useHead({
 
 <template>
   <div class="px-4 space-y-2">
-    <div class="label">Shop</div>
-    <div class="grid grid-cols-3 gap-3">
+   <div class="flex gap-4 label">
+     <div class="cursor-pointer">Shop</div>
+     <div class="cursor-pointer">Inventory</div>
+   </div>
+    <div class="grid grid-cols-1 gap-3">
       <div
           v-for='i in ["cap", "headphone", "knife", "spear"]'
-          class="border rounded"
+          class="border rounded p-3"
       >
-        <div class="pt-full relative">
-          <div class="absolute inset-4 center">
-            <NuxtIcon filled :name="`item/${i}`" class="size-32"/>
+        <div class="flex items-center">
+          <div class="size-24">
+            <NuxtIcon filled :name="`item/${i}`"/>
+          </div>
+          <div class="flex-1 space-y-1">
+            <div class="font-semibold capitalize">{{i}}</div>
+            <div class="text-xs flex gap-1">
+              <div>Buy:</div>
+              <div class="flex items-center">
+                <span>10,000</span>
+                <img src="/icon/thunder.png" class="size-4" alt="">
+              </div>
+              <div class="ml-auto flex items-center gap-1">
+                <NuxtIcon name="storefront" class="size-3"/>
+                <span>0</span>
+              </div>
+            </div>
+            <div class="text-xs flex gap-1">
+              <div>Rent:</div>
+              <div class="flex items-center">
+                <span>100</span>
+                <img src="/icon/thunder.png" class="size-4" alt="">
+              </div>
+              <div class="ml-auto flex items-center gap-1">
+                <NuxtIcon name="storefront" class="size-3"/>
+                <span>0</span>
+              </div>
+            </div>
           </div>
         </div>
-        <div class="p-3 pt-0 space-y-1">
-          <div class="font-semibold text-sm capitalize">{{i}}</div>
-          <div class="text-xs flex justify-between">
-            <div>Buy</div>
-            <div class="flex items-center">
-              <span>10,000</span>
-              <img src="/icon/thunder.png" class="size-4" alt="">
-            </div>
-          </div>
-          <div class="text-xs flex justify-between">
-            <div>Rent</div>
-            <div class="flex items-center">
-              <span>100</span>
-              <img src="/icon/thunder.png" class="size-4" alt="">
-            </div>
-          </div>
+        <div class="grid grid-cols-2 gap-3">
+          <Button disabled class="text-xs" size="sm">Buy</Button>
+          <Button disabled class="text-xs" size="sm" variant="secondary">Rent</Button>
         </div>
       </div>
     </div>
