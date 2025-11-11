607e57fdc0bb6fcaed1d0e9c5b6228ac6d3b0729
Simon <comficker@gmail.com>
2025-11-11 21:19:25 +0700

    Init

--- composables/useStatefulCookie.ts
+++ composables/useStatefulCookie.ts
@@ -4,7 +4,11 @@ export default function useStatefulCookie(name: string) {
   const cfg = useRuntimeConfig()
   const cookie = useCookie(name, {
     maxAge: 60 * 60 * 24 * 7 * 30,
-    ...cfg.public.env === "production" ? {domain: '.pomoduck.com'}: {}
+    ...cfg.public.env === "production" ? {
+      domain: '.pomoduck.com',
+      sameSite: 'lax',
+      secure: true,
+    } : {}
   });
   const state = useState(name, () => cookie.value);
 
